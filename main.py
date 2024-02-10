cd1e9ccbf42e2653d632ab71811c9a3d98e30825
Vhivi <david.angelelli@hotmail.fr>
2024-02-10 14:52:35 +0100

    ** main.py + Ajout de la fonction remove_exif_data() pour supprimer les données EXIF des images. + Ajout de la fonction view_exif_data() pour afficher les données EXIF des images. + Ajout de la fonction main() pour exécuter les fonctions précédente et afficher l'aide.

--- main.py
+++ main.py
@@ -1,20 +1,118 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Description du projet :
-# Auteur :
-# Date de création :
-# Version :
+# Project Description: The project aims to provide a script that removes
+# the EXIF data from an image. EXIF data is information stored
+# in image files. This data can contain sensitive information
+# such as the location of the shot, the camera model, the date
+# of the shot, etc. The script should be able to remove this
+# EXIF data from an image.
+# Author:
+# Creation Date (dd/mm/yyyy): 09/02/2024
+# Version:
 # Python 3.10.11
 
 ################################################################
-# Importation des modules
+# Importing modules
 ################################################################
 
-################################################################
-# Déclaration des variables globales
-################################################################
+import getopt
+import sys
+
+from PIL import ExifTags, Image
 
 ################################################################
-# Déclaration des fonctions
+# Function declarations
 ################################################################
+
+
+def remove_exif_data(image_path):
+    """
+    Removes the EXIF data from the given image.
+
+    Args:
+        image_path (str): The path to the image file.
+
+    Returns:
+        None
+    """
+    print("Remove exif data from image : ", image_path)
+
+    img = Image.open(image_path)
+    # Remove the EXIF data
+    data = list(img.getdata())
+    image_without_exif = Image.new(img.mode, img.size)
+    image_without_exif.putdata(data)
+
+    # Save the image without the EXIF data
+    image_without_exif.save("image_without_exif.jpg")
+    image_without_exif.close()
+
+
+def view_exif_data(image_path):
+    """
+    Displays the EXIF data of an image.
+
+    Args:
+        image_path (str): The path to the image.
+
+    Returns:
+        None
+    """
+    print("View exif data from image : ", image_path)
+
+    img = Image.open(image_path)
+    # Retrieve the EXIF data
+    exif_data = img.getexif()
+    # Display the EXIF data
+    if exif_data is None:
+        print("No exif data found")
+    else:
+        for tag, value in exif_data.items():
+            if tag in ExifTags.TAGS:
+                print(f"{ExifTags.TAGS[tag]}: {value}")
+            else:
+                print(f"Tag: {tag}, value: {value}")
+
+
+def main():
+    """
+    This function is the entry point of the ExifRemovalTool program.
+    It parses the command line arguments and performs the corresponding actions based on the provided options.
+    """
+    argumentlist = sys.argv[1:]
+
+    options = "hv:r:"
+
+    long_options = ["help", "view", "remove"]
+
+    try:
+        arguments, _ = getopt.getopt(argumentlist, options, long_options)
+
+        for currentArgument, currentValue in arguments:
+            # If no argument is passed, display the help message
+            if currentArgument in ("-h", "--help") or len(sys.argv) == 1:
+                print("Help message:", end="\n")
+                print(
+                    "This program is designed to remove EXIF data from an image. It takes the following options:"
+                )
+                print("-h, --help: Display this help message.")
+                print("-v, --view : View the EXIF data of the specified image.")
+                print(
+                    "-r, --remove : Remove the EXIF data from the specified image.",
+                    end="\n\n",
+                )
+                print("Usage: python main.py [options]")
+                print("Example: python main.py -v image.jpg")
+            elif currentArgument in ("-v", "--view"):
+                view_exif_data(currentValue)
+            elif currentArgument in ("-r", "--remove"):
+                remove_exif_data(currentValue)
+    except getopt.GetoptError as err:
+        print(str(err))
+        print("Usage: python main.py [options]")
+        print("Example: python main.py -v image.jpg")
+
+
+if __name__ == "__main__":
+    main()
